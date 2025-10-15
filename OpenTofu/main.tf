4122f23dbd1aa6f1c11894c987eb2b98805c241e
profesantos <66181446+santos-pardos@users.noreply.github.com>
2025-10-15 10:23:00 +0200

    Update main.tf

--- OpenTofu/main.tf
+++ OpenTofu/main.tf
@@ -1,3 +1,18 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "myprimerbucketdaw2025"
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "first-ec2" {
+  ami           = "ami-052064a798f08f0d3"
+  instance_type = "t2.micro"
 }
